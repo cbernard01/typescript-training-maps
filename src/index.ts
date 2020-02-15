7bbcf4aee20bd4b7b0845c7d4b43d0b21a8b6806
Clifford Bernard <clifford@verticalfront.com>
2020-02-14 18:54:14 -0600

    Google Map Integration Commit:
    Integrated Google Maps API and wrapped it with the Map class to privatize Google Map API functions.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-import {User} from "./User";
-import {Company} from "./Company";
+import {Map} from "./Map";
 
-const user = new User();
-const company = new Company();
-
-console.log(user);
-console.log(company);
-
+const map = new Map("map");
